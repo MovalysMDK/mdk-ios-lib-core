b2fc06c9476f83faf0515aa1d7e202424e65da53
Simon Bernardin <simon.bernardin@sopraconsulting.com>
2014-12-12 14:17:52 +0100

    Suppression du script de 'nettoyage' des commentaires

--- updateComments.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# supprime le tag Created by
-find MFCore \( -name "*.m" -o -name "*.h" \) -exec sh -c 'sed "/Created by/d" "$1" > "$1".tmp && mv "$1".tmp "$1"' _ {} \;
-
-#supprime le tag Copyright (c)
-find MFCore \( -name "*.m" -o -name "*.h" \) -exec sh -c 'sed "/Copyright (c)/d" "$1" > "$1".tmp && mv "$1".tmp "$1"' _ {} \;
