c88a37943f723772386ad1897eaf8f6668453b01
Quentin <lagarde.quentin@sopragroup.com>
2015-02-05 14:11:28 +0100

    Fix MFCore

---
+++ MFCore/security/MFCoreSecurity.h
@@ -0,0 +1,18 @@
+//
+//  MFCoreSecurity.h
+//  MFCore
+//
+//  Created by Lagarde Quentin on 08/01/2015.
+//  Copyright (c) 2015 Sopra Consulting. All rights reserved.
+//
+
+#ifndef MFCore_MFCoreSecurity_h
+#define MFCore_MFCoreSecurity_h
+
+
+#import "MFKeychainRunInit.h"
+#import "MFSecurityHelper.h"
+#import "MFSecurityHelperProtocol.h"
+#import "MFSecurityUtils.h"
+
+#endif
